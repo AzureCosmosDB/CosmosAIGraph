f69c4296fb9f85bc62cda3441354c7ce5ef4f5e8
alekseys <alekseys@live.com>
2025-08-24 12:49:46 -0400

    revert: restore cosmos_nosql_service.py and rag_data_service.py to 90d93ee20628a507a12767e75201fdbf363a0c29

--- impl/web_app/src/services/rag_data_service.py
+++ impl/web_app/src/services/rag_data_service.py
@@ -13,7 +13,6 @@ from src.services.strategy_builder import StrategyBuilder
 from src.util.cosmos_doc_filter import CosmosDocFilter
 from src.util.sparql_query_response import SparqlQueryResponse
 from src.util.fs import FS
-from src.util.book_doc_filter import BookDocFilter
 
 # Instances of this class are used to identify and retrieve contextual data
 # in OmniRAG pattern. The data will be read from one or more of the following:
@@ -45,7 +44,7 @@ class RAGDataService:
         except Exception as e:
             logging.critical("Exception in RagDataService#__init__: {}".format(str(e)))
 
-    async def get_rag_data(self, user_text, max_doc_count=20) -> RAGDataResult:
+    async def get_rag_data(self, user_text, max_doc_count=10) -> RAGDataResult:
         """
         Return a RAGDataResult object which contains an array of documents to
         be used as a system prompt of a completion call to Azure OpenAI.
@@ -62,129 +61,50 @@ class RAGDataService:
         sb = StrategyBuilder(self.ai_svc)
         strategy_obj = sb.determine(user_text)
         strategy = strategy_obj["strategy"]
-        #rdr.add_strategy(strategy)
-        #rdr.set_context(strategy_obj["name"])
-    
-        if strategy == "graph" or ConfigService.get_strategy_bypass() == "true":
-            rdr.add_strategy("graph")
-            
-            await self.get_graph_context(user_text, rdr, max_doc_count)
-            # # fall back to vector in case graph doesn't have context
-            # if rdr.has_no_docs():
-            #     rdr.add_strategy("vector")
-            #     await self.get_vector_context(user_text, rdr, max_doc_count)
-            
-        
-        if strategy == "db" or ConfigService.get_strategy_bypass() == "true":
-            rdr.add_strategy("db")
-            
-            await self.get_database_context(user_text, rdr, max_doc_count)
-            
-            # if rdr.has_no_docs():
-            #     rdr.add_strategy("vector")
-            #     await self.get_vector_context(user_text, rdr, max_doc_count)
-        
-        if strategy == "vector" or ConfigService.get_strategy_bypass() == "true":
-            rdr.add_strategy("vector")
-            await self.get_vector_context(user_text, rdr, max_doc_count)
+        rdr.add_strategy(strategy)
+        rdr.set_context(strategy_obj["name"])
+
+        if strategy == "db":
+            name = strategy_obj["name"]
+            rdr.set_attr("name", name)
+            await self.get_database_rag_data(user_text, name, rdr, max_doc_count)
+            if rdr.has_no_docs():
+                rdr.add_strategy("vector")
+                await self.get_vector_rag_data(user_text, rdr, max_doc_count)
+
+        elif strategy == "graph":
+            await self.get_graph_rag_data(user_text, rdr, max_doc_count)
+            if rdr.has_no_docs():
+                rdr.add_strategy("vector")
+                await self.get_vector_rag_data(user_text, rdr, max_doc_count)
+        else:
+            await self.get_vector_rag_data(user_text, rdr, max_doc_count)
 
         rdr.finish()
         return rdr
 
-    # async def get_database_rag_data(
-    #     self, user_text: str, name: str, rdr: RAGDataResult, max_doc_count=10
-    # ) -> None:
-    #     rag_docs_list = list()
-    #     try:
-    #         logging.warning(
-    #             "RagDataService#get_database_rag_data, name: {}, user_text: {}".format(
-    #                 name, user_text
-    #             )
-    #         )
-    #         self.nosql_svc.set_db(ConfigService.graph_source_db())
-    #         self.nosql_svc.set_container(ConfigService.graph_source_container())
-    #         rag_docs_list = await self.nosql_svc.get_documents_by_name([name])
-
-    #         for doc in rag_docs_list:
-    #             rdr.add_doc(self.filtered_cosmosdb_lib_doc(doc))
-
-    #     except Exception as e:
-    #         logging.critical(
-    #             "Exception in RagDataService#get_database_rag_data: {}".format(str(e))
-    #         )
-    #         logging.exception(e, stack_info=True, exc_info=True)
-
-    async def combine_search_results(self, vector_results, fulltext_results):
-        file_names_vector = {item['fileName'] for item in vector_results}
-        file_names_fts = {item['fileName'] for item in fulltext_results}
-    
-        # Union of the two sets
-        union_file_names = file_names_vector.union(file_names_fts)
-    
-        # Convert to sorted list of dicts with only fileName field
-        union_items = [{'fileName': name} for name in sorted(union_file_names)]
-        return union_items
-    
-    async def get_database_context(
-        self, query: str, rdr: RAGDataResult, max_doc_count=10
+    async def get_database_rag_data(
+        self, user_text: str, name: str, rdr: RAGDataResult, max_doc_count=10
     ) -> None:
-        docs_list = list()
+        rag_docs_list = list()
         try:
             logging.warning(
-                "RagDataService#get_database_context, query: {}".format(
-                    query
+                "RagDataService#get_database_rag_data, name: {}, user_text: {}".format(
+                    name, user_text
                 )
             )
             self.nosql_svc.set_db(ConfigService.graph_source_db())
             self.nosql_svc.set_container(ConfigService.graph_source_container())
-            logging.warning(
-                "RagDataService#get_database_context, db: {}, container: {}".format(
-                    ConfigService.graph_source_db(), ConfigService.graph_source_container()
-                )
-            )
-            #docs_list = await self.nosql_svc.query_items(query)
-            
-            resp = self.ai_svc.generate_embeddings(query)
-            search_query_embedded = resp.data[0].embedding            
-            #search_query_embedded = self.ai_svc.generate_embeddings(query)            
-            vector_task = self.nosql_svc.get_vector_search_results(search_query_embedded, top_k=400, threshold=0.8)
-            
-        
-            search_query_arr = query.split(" ")
-            ftquery = f"""
-            SELECT TOP {max_doc_count} c.fileName
-            FROM c
-            ORDER BY RANK FullTextScore(c.text, {search_query_arr})
-            """        
-            logging.warning("query: {}".format(ftquery))
-            
-            
-            fts_task = self.nosql_svc.get_fulltext_search_results(ftquery)
-            items_vector, items_fts = await asyncio.gather(vector_task, fts_task)
-        
-            docs_list = await self.combine_search_results(items_vector, items_fts)
-            
-            # results_list = list()
-            # for item in docs_list:
-            #     cdf = BookDocFilter(item)
-            #     doc = cdf.filter()
-            #     results_list.append(doc)
-            
-            logging.warning(
-                "RagDataService#get_database_context, docs_list count: {}".format(
-                    len(docs_list)
-                )
-            )
-
-            for doc in docs_list:
-                rdr.add_doc(doc)
+            rag_docs_list = await self.nosql_svc.get_documents_by_names([name])
+            pertinent_attributes = "libtype,name, summary, documentation_summary"
+            for doc in rag_docs_list:
+                rdr.add_doc(self.filtered_cosmosdb_lib_doc(doc))
 
         except Exception as e:
             logging.critical(
-                "Exception in RagDataService#get_database_context: {}".format(str(e))
+                "Exception in RagDataService#get_database_rag_data: {}".format(str(e))
             )
             logging.exception(e, stack_info=True, exc_info=True)
-
 
     def filtered_cosmosdb_lib_doc(self, cosmos_db_doc):
         """
@@ -194,12 +114,12 @@ class RAGDataService:
         cdf = CosmosDocFilter(cosmos_db_doc)
         return cdf.filter_for_rag_data()
 
-    async def get_vector_context(
+    async def get_vector_rag_data(
         self, user_text, rdr: RAGDataResult = None, max_doc_count=10
     ) -> None:
         try:
             logging.warning(
-                "RagDataService#get_vector_context, user_text: {}".format(user_text)
+                "RagDataService#get_vector_rag_data, user_text: {}".format(user_text)
             )
             create_embedding_response = self.ai_svc.generate_embeddings(user_text)
             embedding = create_embedding_response.data[0].embedding
@@ -212,16 +132,16 @@ class RAGDataService:
                 rdr.add_doc(vs_doc)
         except Exception as e:
             logging.critical(
-                "Exception in RagDataService#get_vector_context: {}".format(str(e))
+                "Exception in RagDataService#get_vector_rag_data: {}".format(str(e))
             )
             logging.exception(e, stack_info=True, exc_info=True)
 
-    async def get_graph_context(
+    async def get_graph_rag_data(
         self, user_text, rdr: RAGDataResult, max_doc_count=10
     ) -> None:
         try:
             logging.warning(
-                "RagDataService#get_graph_context, user_text: {}".format(user_text)
+                "RagDataService#get_graph_rag_data, user_text: {}".format(user_text)
             )
             # first generate and execute the SPARQL query vs the in-memory RDF graph
             info = dict()
@@ -229,20 +149,20 @@ class RAGDataService:
             info["owl"] = OntologyService().get_owl_content()
             sparql = self.ai_svc.generate_sparql_from_user_prompt(info)["sparql"]
             rdr.set_sparql(sparql)
-            logging.warning("get_graph_context - sparql:\n{}".format(sparql))
+            logging.warning("get_graph_rag_data - sparql:\n{}".format(sparql))
 
             # HTTP POST to the graph microservice to execute the generated SPARQL query
             sqr: SparqlQueryResponse = await self.post_sparql_to_graph_microsvc(sparql)
             FS.write_json(
                 sqr.response_obj,
-                "tmp/get_graph_context_response_obj.json",
+                "tmp/get_graph_rag_data_get_graph_rag_data_response_obj.json",
             )
             for doc in sqr.binding_values():
                 rdr.add_doc(doc)
             FS.write_json(rdr.get_data(), "tmp/rdr.json")
         except Exception as e:
             logging.critical(
-                "Exception in RagDataService#get_graph_context: {}".format(str(e))
+                "Exception in RagDataService#get_graph_rag_data: {}".format(str(e))
             )
             logging.exception(e, stack_info=True, exc_info=True)
 
